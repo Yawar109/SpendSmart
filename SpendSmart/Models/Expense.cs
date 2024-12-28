ca249ff1c385d3d5a1d7b738002c1c5fe8d6544e
Yawar109 <yawara125@gmail.com>
2024-12-28 15:13:13 +0500

    Add project files.

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public Decimal Value { get; set; }
+
+        [Required(ErrorMessage = "na kro bhiee")]
+        public string? Description { get; set; }
+    }
+}
